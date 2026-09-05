087cf504b013fe130fccb4f254815f8ab2ad6724
mor782 <morkehoney@gmail.com>
2026-09-05 03:21:04 +0300

    added json summary

--- runner.js
+++ runner.js
@@ -1,9 +1,24 @@
 import * as sitespeed from 'sitespeed.io';
+import fs from 'fs';
+
+const urlsText = fs.readFileSync('./urls.json', 'utf8');
+const urls = JSON.parse(urlsText);
 
 sitespeed.run({
-    urls: ['https://www.google.com', 'https://www.wikipedia.org'],
+    urls: urls,
     verbose: true,
     browser: 'chrome',
     browsertime: { visualMetrics: false }
 
-}).then((result) => { console.log("Finished running tests!", result) });
+}).then((result) => {console.log("Finished running tests!"); 
+    const summary = result.browsertime.map((page) => ({
+        url: page[0].info.url,
+        ttfb: page[0].timings.ttfb,
+        lcp: page[0].timings.googleWebVitals?.largestContentfulPaint,
+        totalTime: page[0].timings.fullyLoaded
+    }));
+    fs.writeFileSync('./summary.json', JSON.stringify(summary, null, 2));
+});
+
+
+
